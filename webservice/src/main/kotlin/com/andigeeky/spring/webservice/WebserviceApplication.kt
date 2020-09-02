5de70d8beeb768ae87e8dae332183df8e908a4bd
mamatagelanee07 <gelaneem@gmail.com>
2020-09-02 12:17:55 +0100

    [webservice] Initial Commit

---
+++ webservice/src/main/kotlin/com/andigeeky/spring/webservice/WebserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.andigeeky.spring.webservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebserviceApplication>(*args)
+}
---
+++ webservice/src/test/kotlin/com/andigeeky/spring/webservice/WebserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.andigeeky.spring.webservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
