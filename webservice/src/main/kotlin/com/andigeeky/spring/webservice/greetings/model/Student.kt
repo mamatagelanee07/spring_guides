dd74cc6a3e55932561065ffdd26984926d2ad010
mamatagelanee07 <gelaneem@gmail.com>
2020-09-07 15:14:08 +0100

    [ADD] student resource for inserting a student

--- webservice/src/main/kotlin/com/andigeeky/spring/webservice/greetings/model/Student.kt
+++ webservice/src/main/kotlin/com/andigeeky/spring/webservice/greetings/model/Student.kt
@@ -1,11 +1,17 @@
 package com.andigeeky.spring.webservice.greetings.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 
 data class Student(
+    @JsonProperty("id")
     val id: UUID,
+    @JsonProperty("age")
     val age: Int,
+    @JsonProperty("firstName")
     val firstName: String,
+    @JsonProperty("lastName")
     val lastName: String,
+    @JsonProperty("course")
     val course: String
 )
--- webservice/src/main/kotlin/com/andigeeky/spring/webservice/greetings/resource/StudentResource.kt
+++ webservice/src/main/kotlin/com/andigeeky/spring/webservice/greetings/resource/StudentResource.kt
@@ -4,9 +4,8 @@ import com.andigeeky.spring.webservice.greetings.model.Student
 import com.andigeeky.spring.webservice.greetings.service.StudentService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("api/v1/students")
@@ -15,5 +14,13 @@ class StudentResource(@Autowired private val studentService: StudentService) {
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getAllStudents() : List<Student>{
         return studentService.getAllStudents()
+    }
+
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE])
+    fun addStudent(@RequestBody student : Student){
+        studentService.persistNewStudent(
+            studentID = UUID.randomUUID(),
+            student = student
+        )
     }
 }
